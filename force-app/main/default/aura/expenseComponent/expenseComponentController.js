6b6b988928ba40654e5767ec3552750fc9553972
VigneshSalestrip <117450155+VigneshSalestrip@users.noreply.github.com>
2022-12-03 22:47:41 +0530

    spaces

--- force-app/main/default/aura/expenseComponent/expenseComponentController.js
+++ force-app/main/default/aura/expenseComponent/expenseComponentController.js
@@ -1,31 +1,6 @@
 ({
     doinit : function(component, event, helper) 
     {  
-
-     /*
-        var number = component.get("c.getTotalrecords");
-        console.log("Enter 1");
-        
-        number.setCallback(this, function(response) 
-                           {
-                               component.set("v.totalexpenses", response.getReturnValue());
-                           });
-        console.log("Enter 2");
-        $A.enqueueAction(number);
-    
-       var number2 = component.get("c.getData");
-        
-        console.log("Enter 3");
-        number2.setCallback(this, function(response) 
-                           { 
-                               var amountCon= response.getReturnValue();
-                               var convAmount=$A.localizationService.formatCurrency(amountCon);
-
-                               component.set("v.totalamount", convAmount);
-                           });
-        console.log("Enter 4");
-        $A.enqueueAction(number2);
-      */
         // Get Current Month Expense Count 
         var cmonth=component.get("c.getCurrentMonth");
         cmonth.setCallback(this, function(response) 
@@ -38,18 +13,14 @@
         //Get Current & last Month Expense Count Comparison
         var lmonth=component.get("c.getCurrentLastExpenseRecord");
         lmonth.setCallback(this, function(response) 
-                           { 
-                            
+                           {
                                var lastmonthrecord=response.getReturnValue();
                                var convertPercent=$A.localizationService.formatPercent(lastmonthrecord);
                                console.log(convertPercent);
                                if(lastmonthrecord>0){
-                                  
                                   component.set("v.setBoolean","true");
                                   component.set("v.lastMonthRecord",convertPercent);
-
                                }else{
-                                  
                                    component.set("v.setBoolean","false");
                                    component.set("v.lastMonthRecord",convertPercent);
                                }
@@ -73,24 +44,14 @@
                                var lastmonthamount=response.getReturnValue();
                                var convertAmountPercent=$A.localizationService.formatPercent(lastmonthamount);
                                if(lastmonthamount>0){
-                                
                                 component.set("v.amountBoolean","true");
                                 component.set("v.lastMonthAmount",convertAmountPercent);
-                                
                                }else{
-                                
                                 component.set("v.amountBoolean","false");
                                 component.set("v.lastMonthAmount",convertAmountPercent);
-
                                }
                            });
         console.log("Enter 4");
         $A.enqueueAction(lmonthamount);
-        
-
-
-    }
-    
-    
-    
+    }  
 })
